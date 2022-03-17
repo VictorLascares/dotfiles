b44207b9d552e91b7d8ebd4058a7b62619bf653e
Victor Lascares <vmlascares@gmail.com>
2022-03-17 10:09:57 -0600

    Configuraciones de nvim y qtile

---
+++ .config/qtile/settings/widgets.py
@@ -0,0 +1,111 @@
+from libqtile import widget
+from .theme import colors
+
+# Get the icons at https://www.nerdfonts.com/cheat-sheet (you need a Nerd Font)
+
+def base(fg='text', bg='dark'): 
+    return {
+        'foreground': colors[fg],
+        'background': colors[bg]
+    }
+
+
+def separator():
+    return widget.Sep(**base(), linewidth=0, padding=5)
+
+
+def icon(fg='text', bg='dark', fontsize=16, text="?"):
+    return widget.TextBox(
+        **base(fg, bg),
+        fontsize=fontsize,
+        text=text,
+        padding=3
+    )
+
+
+def powerline(fg="light", bg="dark"):
+    return widget.TextBox(
+        **base(fg, bg),
+        text="", # Icon: nf-oct-triangle_left
+        fontsize=37,
+        padding=-3
+    )
+
+
+def workspaces(): 
+    return [
+        separator(),
+        widget.GroupBox(
+            **base(fg='light'),
+            font='UbuntuMono Nerd Font',
+            fontsize=19,
+            margin_y=3,
+            margin_x=0,
+            padding_y=8,
+            padding_x=5,
+            borderwidth=1,
+            active=colors['active'],
+            inactive=colors['inactive'],
+            rounded=False,
+            highlight_method='block',
+            urgent_alert_method='block',
+            urgent_border=colors['urgent'],
+            this_current_screen_border=colors['focus'],
+            this_screen_border=colors['grey'],
+            other_current_screen_border=colors['dark'],
+            other_screen_border=colors['dark'],
+            disable_drag=True
+        ),
+        separator(),
+        widget.WindowName(**base(fg='focus'), fontsize=14, padding=5),
+        separator(),
+    ]
+
+
+primary_widgets = [
+    *workspaces(),
+
+    separator(),
+
+    powerline('color3', 'dark'),
+
+    widget.Systray(background=colors['color3'], padding=5),
+
+    powerline('color2', 'color3'),
+    
+    widget.CurrentLayoutIcon(**base(bg='color2'), scale=0.65),
+
+    widget.CurrentLayout(**base(bg='color2'), padding=5),
+
+    powerline('color1', 'color2'),
+
+    widget.Clock(**base(bg='color1'), format='%d/%m/%Y - %I:%M%p '),
+
+    powerline('dark', 'color1'),
+
+]
+
+secondary_widgets = [
+    *workspaces(),
+
+    separator(),
+
+    powerline('color1', 'dark'),
+
+    widget.CurrentLayoutIcon(**base(bg='color1'), scale=0.65),
+
+    widget.CurrentLayout(**base(bg='color1'), padding=5),
+
+    powerline('color2', 'color1'),
+
+    widget.Clock(**base(bg='color2'), format='%I:%M%p '),
+
+    powerline('dark', 'color2'),
+]
+
+widget_defaults = {
+    'font': 'UbuntuMono Nerd Font Bold',
+    'fontsize': 14,
+    'padding': 1,
+}
+extension_defaults = widget_defaults.copy()
